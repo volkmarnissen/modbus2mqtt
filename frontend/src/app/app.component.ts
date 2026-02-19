3d1e2894ebcbc452539124df3b1f789515f34b50
Volkmar Nissen <volkmar.nissen@gmail.com>
2026-02-19 08:11:33 +0100

    Announcement Banner added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core'
 import { HeaderComponent } from './header/header.component'
 import { RouterModule } from '@angular/router'
+import { AnnouncementBannerComponent } from './announcement-banner/announcement-banner.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule, AnnouncementBannerComponent],
 })
 export class AppComponent {
   title = 'modbus2mqtt'
