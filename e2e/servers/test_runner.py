3f32328a8723c9861813ce4caceebdab1a20ff35
Volkmar Nissen <volkmar.nissen@gmail.com>
2026-02-19 12:42:29 +0100

    Migrate to playwright

--- e2e/servers/test_runner.py
+++ e2e/servers/test_runner.py
@@ -6,17 +6,13 @@ def testRepository(reponame:str):
     print("::group::Unit tests for " + reponame)
     executeCommandWithOutputs(args,sys.stderr, sys.stderr)
     print( '::endgroup::' )
-    args = ["npm", 'run', 'cypress' ]
-    print("::group::Cypress Components tests for " + reponame)
-    executeCommandWithOutputs(args,sys.stderr, sys.stderr)
-    print( '::endgroup::' )
 
 def testall(package:str)->bool:
     testRepository(package)
     import os
-    if os.path.isdir(os.path.join("cypress", "e2e")):
-        print("::group::Cypress run tests")
-        executeCommandWithOutputs(["npx", "cypress", "run"],sys.stderr, sys.stdout)
+    if os.path.isdir("e2e"):
+        print("::group::Playwright E2E tests")
+        executeCommandWithOutputs(["npx", "playwright", "test"],sys.stderr, sys.stdout)
         print( '::endgroup::' )
     else:
-        print("No Cypress e2e tests found in " + os.getcwd(), file=sys.stderr)
+        print("No e2e tests found in " + os.getcwd(), file=sys.stderr)
